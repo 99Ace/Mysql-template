741febd56da6653e002e125566c0ea5460127778
99Ace <ace.ang@trentglobal.edu.sg>
2022-05-11 15:45:14 +0000

    set up base express

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const hbs = require("hbs");
+const wax = require("wax-on");
+
+// Setup dotenv
+require("dotenv").config();
+
+// create an instance of express app
+let app = express();
+
+// set the view engine
+app.set("view engine", "hbs");
+
+// static folder
+app.use(express.static("public"));
+
+// setup wax-on
+wax.on(hbs.handlebars);
+wax.setLayoutPath("./views/layouts");
+
+// enable forms
+app.use(
+    express.urlencoded({
+        extended: false
+    })
+);
+
+async function main() {
+
+    app.get('/', (req,res)=> {
+        res.render('index.hbs')
+    })
+
+
+
+    app.listen(3000, () => {
+        console.log("Server has started");
+    });
+}
+
+main();
